1353453a915575d0c38709478b03156f78f1c94d
babakosuke <work.kosuke.web@gmail.com>
2022-02-15 00:49:10 +0900

    https://www.youtube.com/watch?v=HyU3XL2F9GE 勉強

---
+++ lesson.py
@@ -0,0 +1,28 @@
+# https://www.youtube.com/watch?v=HyU3XL2F9GE&t=2803s
+
+# 実行
+print ("hello")
+
+print("bye")
+
+print("night")
+
+
+# 変数の設定
+num = 1
+print(num)
+
+num01 = 2
+num02 = "hello.world"
+
+# データ型の確認
+print(num01)
+print(type(num02))
+
+a = 1
+b = 23
+
+bool01 = (a < b)
+
+print (bool01)
+
